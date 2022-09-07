d759ecdd79841408dcfc86e3e25dc29778b6633d
Aginius <francesco.barabino@tiscali.it>
2022-09-07 17:14:41 +0200

    Major refactor and update

--- Whatspam.py
+++ Whatspam.py
@@ -5,113 +5,130 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 import time
+import os
 
-def options(inputLine):
-    commands = ["", "", "0", "", "0", "T"]
-    index = 0
-    option = 0
-    for i in range(1,len(inputLine)):
-        if (inputLine[i] == '-msg'):
-            index = 1
-            option = 0
-        elif (inputLine[i] == '-n'):
-            index = 2
-            option = 0
-        elif (inputLine[i] == '-to'):
-            index = 3
-            option = 0
-        elif (inputLine[i] == '-id'):
-            index = 4
-            option = 0
-        elif (inputLine[i] == '-sm'):
-            commands[5] = "F"
-            option = 0
-        elif (option):
-            commands[index] += " " + inputLine[i]
+
+#The class names of useful html divs and spans, they are often changed by Meta
+
+MESSAGE_FIELD = 'fd365im1 to2l77zo bbv8nyr4 mwp4sxku gfz4du6o ag5g9lrv'
+CONTACT = 'ggj6brxn gfz4du6o r7fjleex g0rxnol2 lhj4utae le5p0ye3 l7jjieqr i0jNr'
+SEARCH_FIELD = '_13NKt copyable-text selectable-text'
+
+class SpamSession:
+    def __init__(self, name, message, spams, delay):
+        self.name = name
+        self.message = message
+        self.spams = spams
+        self.delay = delay
+
+    def spam(self):
+        getContact(self.name).click()
+        textField = browser.find_element(By.XPATH, "//div[@class='%s' and @data-tab='10']" %(MESSAGE_FIELD))
+
+        if (self.delay > 0):
+            delay = self.delay / 1000;
+            textField.clear()
+            textField.send_keys(spamSession.message)
+            textField.send_keys(Keys.ENTER)
+            for i in range(spamSession.spams):
+                time.sleep(delay)
+                textField.clear()
+                textField.send_keys(spamSession.message)
+                textField.send_keys(Keys.ENTER)
+
         else:
-            commands[index] = inputLine[i]
-            option = 1
-    return commands
+            for i in range(spamSession.spams):
+                textField.clear()
+                textField.send_keys(spamSession.message)
+                textField.send_keys(Keys.ENTER)
 
-commands = options(argv)
 
-main = commands[0]
+def displayHelp():
 
-while (main == ""):
-    print("Insert a command, or type help")
-    main = input()
-
-if (main == "spam"):
-
-    msg = commands[1]
-    spams = int(commands[2])
-    name = commands[3]
-    index = int(commands[4])-1
-
-    if (commands[5] == 'T'):
-        safemode = True
-    elif (commands[5] == 'F'):
-        safemode = False
-    while (spams < 1):
-        print("Insert a spam number (it must be greater than 0)")
-        spams = int(input())
-    while (name == "" and index == -1):
-        print("Insert the target name")
-        name = input()
-    while (msg == ""):
-        print("Insert a message")
-        msg = input()
-
-    #CHANGE THIS VALUE TO SET YOUR BROWSER AND LOCATE YOUR WEBDRIVER (you can download the webdriver from the links at https://selenium-python.readthedocs.io/installation.html)
-    #Currently using a Firefox webdriver located in /usr/bin
-    browser = wb.Firefox()
-
-    browser.get("https://web.whatsapp.com/")
-    #Wating the QR Code scan
-    wait(browser, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "_2wP_Y")))
-    try:
-        if (name != ""):    
-            browser.find_element_by_xpath('//span[@class="_1wjpf _3NFp9 _3FXB1" and @title="%s"]' %(name)).click()
-        else:
-            browser.find_element_by_xpath('//div[@class="_2wP_Y" and contains(@style,"translateY(%dpx);")]' %(ind*72)).click()
-    except:
-        print("Target not found")
-    else: 
-        if (safemode): 
-            print("PRESS ENTER TO START SPAMMING")
-            input()
-
-        textFields = browser.find_elements_by_xpath('//div[@class="_2S1VP copyable-text selectable-text"]')
-        buttons = browser.find_elements_by_class_name('weEq5')
-
-        for i in range(spams):
-            print("Sending",msg)
-            textFields[1].send_keys(msg)
-            buttons[1].click()
-
-        print("Successfully sent %d spams" % (spams))
-        browser.close()
-    
-elif (main == "help"):
-    print("Whatsapp spammer lets you send multiple messages at a time in whatsapp")
+    print("""
+ __    __ _           _                             
+/ / /\ \ \ |__   __ _| |_ ___ _ __   __ _ _ __ ___  
+\ \/  \/ / '_ \ / _` | __/ __| '_ \ / _` | '_ ` _ \ 
+ \  /\  /| | | | (_| | |_\__ \ |_) | (_| | | | | | |
+  \/  \/ |_| |_|\__,_|\__|___/ .__/ \__,_|_| |_| |_|
+                             |_|                    
+""")
+    print("Whatspam lets you send multiple messages at a time in Whatsapp.")
     print("Once started, it will open your browser and require you to scan the QR code")
-    print("with your phone, after this the script will do the rest")
+    print("with your phone, after this the script will do the rest.")
     print()
-    print("COMMANDS:")
-    print("spam | Starts spamming, check options below")
-    print("help | Shows this screen")
+    print("Currently supports Google Chrome, requires the chromedriver to be in the same directory\nof the script and to be the same version of your browser.")
     print()
     print("OPTIONS:")
-    print("Mandatory:")
-    print('-msg | The message to send, example: spam -msg My message')
+    print('-msg | The message to send')
     print("-to | The name of the target")
-    print("-id | As an alternative to the name, the index of the target, starting from the top of your chats")
     print("-n | The number of spams to send")
+    print()
     print("Optional:")
-    print("-sm | Just type it to deactivate safe mode, which requires a confirm before sending the spam")
+    print("-d | The delay between each message in milliseconds")
 
-else:
-    print("Unknown command")
+def options(inputLine):
+    name = ""
+    message = ""
+    spams = 0
+    delay = 0
 
-print()
+    for i in range(1, len(inputLine)):
+        if (inputLine[i] == '-msg'):
+            message = inputLine[i + 1]
+        elif (inputLine[i] == '-n'):
+            spams = int(inputLine[i + 1])
+        elif (inputLine[i] == '-to'):
+            name = inputLine[i + 1]
+        elif (inputLine[i] == '-d'):
+            delay = int(inputLine[i + 1])
+        i += 1
+
+    if (name == "" or message == "" or spams <= 0 or delay < 0):
+        raise(Exception())
+
+    return SpamSession(name, message, spams, delay)
+
+def getContact(name):
+    try:
+        return browser.find_element(By.XPATH, "//span[@class='%s' and @title='%s']" %(CONTACT, name))
+    except:
+        return browser.find_element(By.XPATH, "//span[@class='%s' and contains(@title, '%s')]" %(CONTACT, name))
+
+
+
+spamSession = None
+try:
+    spamSession = options(argv)
+except:
+    displayHelp()
+    exit()
+
+browser = wb.Chrome(executable_path = os.getcwd() + '/chromedriver.exe')
+
+browser.get("https://web.whatsapp.com/")
+
+#Wating the QR Code scan
+wait(browser, 50).until(EC.presence_of_element_located((By.XPATH, "//span[@class='%s']" %(CONTACT))))
+
+
+try:
+    spamSession.spam()
+except:
+    searchBar = browser.find_element(By.XPATH, "//div[@class='%s' and @data-tab='3']" %(SEARCH_FIELD))
+    searchBar.clear()
+    searchBar.send_keys(spamSession.name)
+    
+    wait(browser, 30).until(EC.presence_of_element_located((By.XPATH, "//span[@class='%s']" %(CONTACT))))
+
+    spamSession.spam()
+
+
+print("All message sent, press ENTER to close the browser")
+input()
+
+
+
+
